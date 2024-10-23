4c74c6bf8de2f647c5e970e4e3ebebea956c295f
damhiya <damhiya@gmail.com>
2024-10-23 11:01:42 +0900

    use monoid instead of monad

--- Shakefile.hs
+++ Shakefile.hs
@@ -7,10 +7,10 @@ translator :: String
 translator = "bin/translator"
 
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-    want ["all"]
+main = shakeArgs shakeOptions{shakeFiles="_build"} $ mconcat
+  [ want ["all"]
 
-    "all" ~> do
+  , "all" ~> do
       metaSrcs <- getDirectoryFiles "src/meta" ["*.md"]
       wikiSrcs <- getDirectoryFiles "src/wiki" ["*.md"]
       cssSrcs  <- getDirectoryFiles "src/css"  ["*.css"]
@@ -19,13 +19,13 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
           ++ ["site/wiki" </> f -<.> "html" | f <- wikiSrcs]
           ++ ["site/css"  </> f             | f <- cssSrcs]
 
-    translator %> \out -> do
+  , translator %> \out -> do
         translatorSrcs <- getDirectoryFiles "" ["translator/**.hs"]
         need translatorSrcs
         cmd_ "mkdir" "-p" "bin"
         cmd_ "ghc" "translator/Main.hs" "-Wall" "-O3" "-outputdir=_build/translator" "-o" out
 
-    "site/index.html" %> \_ -> do
+  , "site/index.html" %> \_ -> do
         metaSrcs <- getDirectoryFiles "" ["src/meta/*.md"]
         wikiSrcs <- getDirectoryFiles "" ["src/wiki/*.md"]
         need $ [translator]
@@ -33,18 +33,20 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
             ++ wikiSrcs
         cmd_ translator "--index" "-i" "src" "-o" "site"
 
-    "site/meta/*.html" %> \out -> do
+  , "site/meta/*.html" %> \out -> do
         let src = "src/meta" </> takeBaseName out <.> "md"
         need [src, "src/bibliography.bib", translator]
         cmd_ translator "--meta" [takeBaseName src] "-i" "src" "-o" "site"
 
-    "site/wiki/*.html" %> \out -> do
+  , "site/wiki/*.html" %> \out -> do
         let src = "src/wiki" </> takeBaseName out <.> "md"
         need [src, "src/bibliography.bib", translator]
         cmd_ translator "--wiki" [takeBaseName src] "-i" "src" "-o" "site"
 
-    "site/css/*.css" %> \out -> do
+  , "site/css/*.css" %> \out -> do
         let src = replaceDirectory1 out "src"
         need [src]
         cmd_ "mkdir" "-p" "site/css"
         cmd_ "cp" src out
+
+  ]
